bb86aad77f24ebc252e8d8a05aa95fc64afd5ed8
John Hollowell <jhollowe@johnhollowell.com>
2023-07-13 06:18:12 +0000

    WIP

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"log"
+
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.NewWithID("com.jhollowe.hdhr-signal-utils")
-	w := a.NewWindow("Hello World")
+	w := a.NewWindow("HDHomeRun Signal Utils")
 
-	w.SetContent(widget.NewLabel("Hello World!"))
+	// discoveredSelector := widget.NewSelect([]string{"Option1", "Option2"}, func(option string) { log.Println("selected: ", option) })
+	discoverButton := widget.NewButton("Discover Tuners", func() { log.Println("discover button pressed") })
+
+	w.SetContent(discoverButton)
+	// w.SetContent(discoveredSelector)
+
 	w.ShowAndRun()
 }
